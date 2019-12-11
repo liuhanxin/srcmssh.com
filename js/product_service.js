0d66a697d0bbbf472d23a2ec45ee10416e274743
liuhanxin <17674003964@163.com>
2019-12-11 11:14:14 +0800

    First Commit

---
+++ js/product_service.js
@@ -0,0 +1,55 @@
+// 视频制作
+$("#spzz").mouseenter(function() {
+	
+	$("#spzzdiv img").css("background-color","#000000bf");
+});
+// 视频制作
+$("#spzz").mouseleave(function() {
+	$("#spzzdiv img").css("background-color","#00000000");
+});
+
+// 设计服务
+$("#sjfw").mouseenter(function() {
+	
+	$("#sjfwdiv img").css("background-color","#000000bf");
+});
+// 设计服务
+$("#sjfw").mouseleave(function() {
+	$("#sjfwdiv img").css("background-color","#00000000");
+});
+
+// 软件制作
+$("#rjzz").mouseenter(function() {
+	$("#rjzzdiv img").css("background-color","#000000bf");
+});
+// 软件制作
+$("#rjzz").mouseleave(function() {
+	$("#rjzzdiv img").css("background-color","#00000000");
+});
+
+// 活动宣传
+$("#hdxc").mouseenter(function() {
+	$("#hdxcdiv img").css("background-color","#000000bf");
+});
+// 活动宣传
+$("#hdxc").mouseleave(function() {
+	$("#hdxcdiv img").css("background-color","#00000000");
+});
+
+// 航拍
+$("#hp").mouseenter(function() {
+	$("#hpdiv img").css("background-color","#000000bf");
+});
+// 航拍
+$("#hp").mouseleave(function() {
+	$("#hpdiv img").css("background-color","#00000000");
+});
+
+// 微信运营
+$("#wxyy").mouseenter(function() {
+	$("#wxyydiv img").css("background-color","#000000bf");
+});
+// 微信运营
+$("#wxyy").mouseleave(function() {
+	$("#wxyydiv img").css("background-color","#00000000");
+});
